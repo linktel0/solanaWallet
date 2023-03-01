9a1b235016acb5be163b590850c09e6e921b6f01
linktel0 <linktel@163.com>
2023-03-01 18:24:44 -0300

    fixed color theme error

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,18 @@
-module.exports = {
-  resolver: {
-    sourceExts: ["jsx", "js", "ts", "tsx", "cjs",'json'],
-    extraNodeModules: {
-      stream: require.resolve("readable-stream"),
-    },
-  },
-};
+const { getDefaultConfig } = require('expo/metro-config');
 
+// extra config is needed to enable `react-native-svg-transformer`
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig(__dirname);
+  return {
+    transformer: {
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+    },
+    resolver: {
+      extraNodeModules: {
+        stream: require.resolve("readable-stream"),
+      },
+    },
+  };
+})();
